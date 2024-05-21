4eb4388ffbf82afafeb80fa50207ed485639a3d4
Mitch J <mjacovet@kent.edu>
2024-05-20 21:53:14 -0400

    use webhooks to send messages

--- bot.py
+++ bot.py
@@ -2,11 +2,12 @@ import asyncio
 from datetime import datetime
 import json
 from urllib.parse import urlencode
+import aiohttp
 
 import dateutil
 import discord
 
-from email_viewer import email_view_server
+from email_server import email_view_server
 from config import config
 from email_scraper import *
 
@@ -27,63 +28,55 @@ def get_email_metadata_embed(email_meta, email_url):
     return discord.Embed(
         title=email_meta.subject,
         description=f"*from* {email_meta.from_address}",
-        url=get_email_viewer_url(email_url),
+        url=get_email_viewer_url(email_url) if email_meta.has_html else email_url,
         timestamp=email_meta.timestamp
     )
 
 
-client = discord.Client(intents=discord.Intents.default())
-
-@client.event
-async def on_ready():
+async def check_mail_forever():
     global config
 
-    print(f'We have logged in as {client.user} at {formatted_current_time()}')
+    async with aiohttp.ClientSession() as webhook_session:
 
-    session = login(config["email_password"])
-    last_email_url = get_last_email_url(get_recent_email_urls(session))
-    last_email_id = email_url_to_id(last_email_url)
-    print(f"will alert for emails with IDs greater than {last_email_id}")
+        webhook = discord.Webhook.from_url(config["webhook_url"], session=webhook_session)
 
-    email_meta = get_email_metadata(session, last_email_url)
-    discord_embed = get_email_metadata_embed(email_meta, last_email_url)
-    await (
-        client
-            .get_channel(int(config["output_channel"]))
-            .send("Email bot is active. Most recent email in inbox is:", embed=discord_embed)
-    )
+        listmail_session = login(config["email_password"])
+        last_email_url = get_last_email_url(get_recent_email_urls(listmail_session))
+        last_email_id = email_url_to_id(last_email_url)
+        print(f"will alert for emails with IDs greater than {last_email_id}")
 
-    if "release_notes" in config and len(config["release_notes"]):
-        await (
-            client
-                .get_channel(int(config["output_channel"]))
-                .send("Release notes: " + config["release_notes"])
+        email_meta = get_email_metadata(listmail_session, last_email_url)
+        discord_embed = get_email_metadata_embed(email_meta, last_email_url)
+        await webhook.send(
+            "Email bot is active. Most recent email in inbox is:",
+            embed=discord_embed
         )
 
+        if "release_notes" in config and len(config["release_notes"]):
+            await webhook.send("Release notes: " + config["release_notes"])
+
     while True:
-        # reload config in case it changed
-        with open("private.json", encoding="utf-8") as config_file:
-            config = json.load(config_file)
-        
-        # check for emails newer than the last one observed
-        session = login(config["email_password"])
-        emails = get_recent_email_urls(session)
-        for email_url in emails:
-            if email_url_to_id(email_url) > last_email_id:
-                email_meta = get_email_metadata(session, email_url)
-                discord_embed = get_email_metadata_embed(email_meta, email_url)
-                print(f"sending message for email {email_url} at {formatted_current_time()}")
-                await (
-                    client
-                       .get_channel(int(config["output_channel"]))
-                       .send("You've got mail!", embed=discord_embed)
-                )
-        
-        # update ID of the last email observed
-        new_last_email_id = email_url_to_id(get_last_email_url(emails))
-        if new_last_email_id != last_email_id:
-            print(f"will now alert for emails with IDs greater than {new_last_email_id}")
-            last_email_id = new_last_email_id
+        async with aiohttp.ClientSession() as webhook_session:
+
+            # reload config in case it changed
+            with open("private.json", encoding="utf-8") as config_file:
+                config = json.load(config_file)
+            
+            # check for emails newer than the last one observed
+            listmail_session = login(config["email_password"])
+            emails = get_recent_email_urls(listmail_session)
+            for email_url in emails:
+                if email_url_to_id(email_url) > last_email_id:
+                    email_meta = get_email_metadata(listmail_session, email_url)
+                    discord_embed = get_email_metadata_embed(email_meta, email_url)
+                    print(f"sending message for email {email_url} at {formatted_current_time()}")
+                    await webhook.send("You've got mail!", embed=discord_embed)
+            
+            # update ID of the last email observed
+            new_last_email_id = email_url_to_id(get_last_email_url(emails))
+            if new_last_email_id != last_email_id:
+                print(f"will now alert for emails with IDs greater than {new_last_email_id}")
+                last_email_id = new_last_email_id
 
         # check again in 3 minutes
         await asyncio.sleep(60 * 3)
@@ -93,7 +86,7 @@ async def main():
     email_view_server.listen(8888)
     print("email viewer listening on port 8888")
     print("starting discord bot")
-    await asyncio.create_task(client.start(config["discord_token"]))
+    await asyncio.create_task(check_mail_forever())
 
 
 if __name__ == "__main__":
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import json
 
 with open("private.json", encoding="utf-8") as config_file:
     config = json.load(config_file)
-    expected_keys = ("discord_token", "email_password", "output_channel", "viewer_host")
+    expected_keys = ("email_password", "webhook_url", "viewer_host")
     for key in expected_keys:
         assert key in config, f"missing {key} from private.json!"
     allowed_keys = (*expected_keys, "release_notes")
